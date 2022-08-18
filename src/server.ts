9de06304b40910d82b020732634a2eb634f495c6
julianbarnes <julian.barnes100@gmail.com>
2022-08-18 08:54:40 -0400

    Adds json body parser to server.ts

--- src/server.ts
+++ src/server.ts
@@ -10,6 +10,7 @@ const app = express();
 const port = process.env.PORT || 8000;
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(cors());
 
 //NOSQL Database
