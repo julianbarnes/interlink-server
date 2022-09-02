2e9a7076beaef5aa0d2d824c53aa56b978648b03
julianbarnes <julian.barnes100@gmail.com>
2022-09-02 16:09:55 -0400

    Adds local upload

--- src/managers/aws-manager.ts
+++ src/managers/aws-manager.ts
@@ -3,6 +3,8 @@ var multer  = require('multer')
 var multerS3 = require('multer-s3')
 var upload = multer({ dest: 'uploads/' })
 const AWS = require('aws-sdk');
+const fs = require('fs');
+const { Readable } = require('stream');
 
 /**
  * @class Aws
@@ -13,42 +15,34 @@ const AWS = require('aws-sdk');
     private static filename: string = 'src/managers/aws-manager.ts';
     private static s3: any;
     public static start() {
-        AWS.s3 = new AWS.S3({
+        this.s3 = new AWS.S3({
             accessKeyId: process.env.AWS_ACCESS_KEY,
             secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
         });
     }
-    public static async upload(title: string, object: any): Promise<ManagedUpload> {
+    public static async upload(title: string, data: any): Promise<ManagedUpload> {
         this.start();
         let date = new Date();
         const params = {
             Bucket: 'interlink-app',
-            Key: `${title}-${date.toISOString}.jpg`,
-            Body: object
-        };
-          //results contains the progress of the upload
-        //return Aws.s3.upload(params);
-        // return AWS.s3.upload(params, function(err, results) {
-        //     if (err) {
-        //         console.log(AWS.filename + " " + err.message)
-        //     }
-        
-        //     console.log(`File uploaded succesfully at ${results.Location}`)
-        //     return results;
-        // });
+            Key: `${title}.jpg`,
+            Body: Readable.from(data)
 
-        const upload = await multer({
-            storage: multerS3({
-                s3: this.s3,
-                bucket: 'interlink-app',
-                metadata: function (req, file, cb) {
-                    cb(null, {fieldName: 'TESTING_META_DATA!'});
-                },
-                key: function (req, file, cb) {
-                    cb(null, Date.now().toString())
-                }
-            })
-        })
-        return upload;
+        };
+
+
+
+        try {
+            fs.writeFileSync(`uploads/${title}`, data)
+        }
+        catch(err)  {
+            if (err) {
+                console.error(err);
+            }
+        }
+        finally {
+            return this.s3.upload(params).promise();
+        }
+
     }
  }
